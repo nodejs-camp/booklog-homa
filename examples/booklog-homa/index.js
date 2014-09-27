99ae11d7ffb521bbad029e6da38df01c23922da8
HomaD <homelome@hotmail.com>
2014-09-27 10:58:28 +0800

    add passport script and login facebook module, #1

--- examples/booklog-homa/index.js
+++ examples/booklog-homa/index.js
@@ -56,6 +56,35 @@ var posts = [{
 
 var bodyParser = require('body-parser');
 
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: "573894422722633",
+    clientSecret: "cd295293760fb7fe56a69c1aae66da51",
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
